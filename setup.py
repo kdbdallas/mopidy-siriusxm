7b73c29c00df8850f368795921a40319e5d0b40a
Dallas Brown <dbrown@hashbangind.com>
2015-12-28 09:01:15 -0700

    Trying a dependency link to pysiriusxm

--- setup.py
+++ setup.py
@@ -27,7 +27,10 @@ setup(
         'setuptools',
         'Mopidy >= 1.0',
         'Pykka >= 1.1',
-        'pysiriusxm >= 0.1.0',
+        'pysiriusxm == 0.1.0',
+    ],
+    dependency_links=[
+        'https://github.com/kdbdallas/pysiriusxm.git#egg=pysiriusxm-0.1.0',
     ],
     entry_points={
         'mopidy.ext': [
